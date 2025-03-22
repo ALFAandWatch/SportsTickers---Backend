db16af6b65f200b74328ff730b432fa33e77a1e5
Alfonso <alfonso.gonzalezwebdev@gmail.com>
2025-03-22 18:18:44 -0300

    Trying to fix envs try #1

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,8 +3,4 @@ import 'dotenv/config';
 export const PORT = process.env.PORT; //PORT FOR APP TO LISTEN
 
 /* DATABASE ENV */
-export const HOST = process.env.HOST;
-export const PORT_PG = process.env.PORT_PG;
-export const USERNAME_PG = process.env.USERNAME_PG;
-export const PASSWORD = process.env.PASSWORD;
-export const DATABASE = process.env.DATABASE;
+export const DATABASE_URL = process.env.DATABASE_URL;
