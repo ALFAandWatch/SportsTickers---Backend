ac4fa3928a97f8fc739bd72fa36514425158578c
Alfonso <alfonso.gonzalezwebdev@gmail.com>
2025-04-27 19:55:34 -0300

    added the cors thing

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,14 @@ import morgan from 'morgan';
 import cors from 'cors';
 
 const server = express();
+const allowedOrigins = ['https://sport-tickers-j9k5.vercel.app'];
 
 server.use(morgan('dev'));
 server.use(express.json());
 
 server.use(
    cors({
-      origin: 'https://sport-tickers-j9k5.vercel.app/',
-      credentials: true,
+      origin: allowedOrigins,
    })
 );
 
